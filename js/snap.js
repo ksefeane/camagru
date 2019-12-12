617db1581bc13c123ef224960ce5fddb90ae31e3
gradez <ksefeane@student.wethinkcode.coza>
2019-12-12 12:37:07 +0200

    filters working

--- js/snap.js
+++ js/snap.js
@@ -3,17 +3,27 @@ const snap = document.getElementById('snap');
 const canvas = document.getElementById('edit');
 const save = document.getElementById('save');
 const open = document.getElementById('open');
-const apply = document.getElementById('apply');
-const friday = document.getElementById('friday');
-			
-var context = canvas.getContext('2d');
 
+const friday = document.getElementById('friday');
+const insta = document.getElementById('insta');
+const twitter = document.getElementById('twitter');
+const iphone = document.getElementById('iphone');
+const tiktok = document.getElementById('tiktok');
+
+var context = canvas.getContext('2d');
 
 open.addEventListener("click", feed);
 snap.addEventListener("click", takeSnap);
-friday.addEventListener("click", applySticker);
+
+friday.addEventListener("click", applyfriday);
+insta.addEventListener("click", applyinsta);
+twitter.addEventListener("click", applytwitter);
+iphone.addEventListener("click", applyiphone);
+tiktok.addEventListener("click", applytiktok);
+
 save.addEventListener("click", saveSnap);
-feed();
+//feed();
+
 function feed() {
 	var constraints = {video: {width: 500, height: 500}};
 	navigator.mediaDevices.getUserMedia(constraints)
@@ -24,9 +34,15 @@ function takeSnap () {
 	context.drawImage(video, 0, 0, 500, 500);
 }
 
-function applySticker () {
-	context.drawImage(friday, 0, 340, 150, 150);
-}
+function applyfriday () {context.drawImage(friday, 0, 340, 150, 150);}
+function applyinsta () {context.drawImage(insta, 0, 340, 150, 150);}
+function applytwitter () {context.drawImage(twitter, 0, 340, 150, 150);}
+function applyiphone () {context.drawImage(iphone, 0, 340, 150, 150);}
+function applytiktok () {context.drawImage(tiktok, 0, 340, 150, 150);}
+
+
+
+
 
 function saveSnap () {
 	var data = canvas.toDataURL();
