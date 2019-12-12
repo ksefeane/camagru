4316391b6d775c1c45a006a109626b12c0021a25
gradez <ksefeane@student.wethinkcode.coza>
2019-12-12 22:05:38 +0200

    fixed that nigga bugs..are you kidney me

--- login.php
+++ login.php
@@ -39,12 +39,12 @@ if (isset($_SESSION['usertoken'])) {
 			$strong = "True";
 			$user_id = DB::query('SELECT id FROM users WHERE username=:username', array('username'=>$username))[0]['id'];
 			$token = bin2hex(openssl_random_pseudo_bytes(64, $strong));
-			DB::query('INSERT INTO tokens (user_id, token) VALUES (:user_id, :token)', array(':user_id'=>$user_id, ':token'=>sha1($token)));
+			DB::query('INSERT INTO tokens (user_id, username, token) VALUES (:user_id, :username, :token)', array(':user_id'=>$user_id, ':username'=>$username, ':token'=>sha1($token)));
 			$_SESSION['usertoken'] = $token;
 			$_SESSION['username'] = $username;
 			setcookie("SID", $_SESSION['usertoken'], time() + 60 * 60 * 24 * 1);
 			echo "login successful";
-			header('location: feed.php');
+			header('location: ft_snapchat.php');
 		} else {echo "password incorrect";}
 	} else {echo "user not found";}
 } else {echo "logged out";}
